1c4f671562a5cd837bc5113a3114b6ba12f36e91
Makhalov Maksim <makhalovmaksym@gmail.com>
2023-10-11 19:07:59 +0300

    add: logo and 404 page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapperNav}>
-        <div className={styles.logo}><Link to="/">Logo</Link></div>
+        <div><Link to="/" className={styles.logo}></Link></div>
         <Navigation />
       </div>
     </div>
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,23 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import styles from './NotFoundPage.module.scss'
 
 const NotFoundPage = () => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>Сторінка незнайдена :(</h1>
-        <p className={styles.text}>Поверніться до головної <Link to="/" className={styles.link}>сторінки</Link></p>
+        <h1 className={styles.title}>error</h1>
+        <div className={styles.content}>
+
+             <div className={styles.errCodeOne}></div>
+
+             <div className={styles.errCodeTwo}></div>
+   
+          <p className={styles.text}>сторінка не знайдена</p>
+        </div>
+        <button className={styles.linkBtn}>
+          <a href="/" className={styles.link}>
+          На головну
+          </a>
+        </button>
     </div>
   )
 }
