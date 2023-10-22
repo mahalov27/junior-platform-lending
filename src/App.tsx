aa5b0bd708d0ffce7b2ac7caa965359e655d1adc
Makhalov Maksim <makhalovmaksym@gmail.com>
2023-10-22 20:58:41 +0300

    add: main loader for layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { Suspense } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
+import MainLoader from "./components/MainLoader/MainLoader";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 const App: React.FC = () => {
   const location = useLocation();
 
   return (
-    <Suspense fallback={<p>Loader...</p>}>
+    <Suspense fallback={<p><MainLoader/></p>}>
       <Routes key={location.pathname} location={location}>
         <Route path="/" element={<Layout />}>
           <Route index element={<AboutUsPage />} />
