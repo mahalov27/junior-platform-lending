ce226d4a2b2026d71db864bf260d693afacb8397
Makhalov Maksim <makhalovmaksym@gmail.com>
2023-11-08 12:11:19 +0200

    add: translation for not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,25 +1,26 @@
-import { Link } from 'react-router-dom'
-import styles from './NotFoundPage.module.scss'
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import styles from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>error</h1>
-        <div className={styles.content}>
+      <h1 className={styles.title}>error</h1>
+      <div className={styles.content}>
+        <div className={styles.errCodeOne}></div>
 
-             <div className={styles.errCodeOne}></div>
+        <div className={styles.errCodeTwo}></div>
 
-             <div className={styles.errCodeTwo}></div>
-   
-          <p className={styles.text}>сторінка не знайдена</p>
-        </div>
-        <button className={styles.linkBtn}>
-          <Link to="/" className={styles.link}>
-          На головну
-          </Link>
-        </button>
+        <p className={styles.text}> {t("404.title")}</p>
+      </div>
+      <button className={styles.linkBtn}>
+        <Link to="/" className={styles.link}>
+          {t("404.btn")}
+        </Link>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
